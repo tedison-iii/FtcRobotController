095c34c66f2b2cee289cd8b4d07f74a60e9d8afc
edsan <vazqueE22@pwcs-edu.org>
2021-11-30 14:01:47 -0500

    I deleted a file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HWMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HWMap.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -11,7 +12,8 @@ public class HWMap {
     public DcMotor backRightMotor = null;
     public DcMotor intakeRight = null;
     public DcMotor intakeLeft = null;
-    public DcMotor armMotor = null;
+    public DcMotor armMotor1 = null;
+    public DcMotor armMotor2 = null;
 
     //create servo
     public Servo testServo = null;
@@ -27,10 +29,26 @@ public class HWMap {
     private void initialize(HardwareMap CMap) {
         hardwareMap =CMap;
 
-        //backLeftMotor = hardwareMap.get(DcMotor.class,deviceName:"");
+        //connect motors
+
+        //backLeftMotor = hardwareMap.get(DcMotor.class, deviceName:"backLeft");
         //backRightMotor = hardwareMap.get(DcMotor.class,deviceName:"");
         //intakeRight = hardwareMap.get(DcMotor.class,deviceName:"");
         //intakeLeft = hardwareMap.get(DcMotor.class,deviceName:"");
         //armMotor = hardwareMap.get(DcMotor.class,deviceName:"");
+
+        //connect servos
+
+        // set up motor direction
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        // set ZERO POWER BEHAVIOR
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // set motors to use no power
+        backRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
     }
 }
